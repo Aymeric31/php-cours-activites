c23750639770d8b90afcc12a4c709fe113dd3fc9
Aymeric <aymeric.gioia@hotmail.fr>
2017-04-05 23:26:51 +0200

    modif array

--- array/array.php
+++ array/array.php
@@ -43,9 +43,9 @@ echo '<br><br>';
 //------------------------------------
 echo 'Tableau multidimensionnel <br>';
 $vehicules = [
-  ['Twingo', 'Aygo', 'C1', '107'],
-  ['Clio', 'Golf', 'i30', '308'],
-  ['Freelander', '3008', 'Santa fe', 'Sportage'],
+['Twingo', 'Aygo', 'C1', '107'],
+['Clio', 'Golf', 'i30', '308'],
+['Freelander', '3008', 'Santa fe', 'Sportage'],
 ];
 print_r($vehicules);
 echo '<br>';
@@ -67,7 +67,7 @@ $vehicule = array(
   'marque' => 'Lamborghini',
   'puissance' => 700,
   'prix' => 200000,
-);
+  );
 /*
  * On aurait pu déclarer le tableau de cette façon aussi :
  * $vehicule = [
@@ -92,30 +92,30 @@ echo 'Tableau associatif multidimensionnel : <br>';
 $vehiculeConcession = array(
   'Bas de gamme' => array(
     'nom' => 'C1',
-       'marque' => 'Citroen',
-       'puissance' => 70,
-       'prix' => 10000,
-  ),
+    'marque' => 'Citroen',
+    'puissance' => 70,
+    'prix' => 10000,
+    ),
   'Milieu de gamme' => array(
     'nom' => 'Golf',
-       'marque' => 'VW',
-       'puissance' => 140,
-       'prix' => 270000,
-  ),
+    'marque' => 'VW',
+    'puissance' => 140,
+    'prix' => 270000,
+    ),
   'Haut de gamme' => array(
     'nom' => 'Aventador LP 700-4',
-       'marque' => 'Lamborghini',
-       'puissance' => 700,
-       'prix' => 200000,
-  ),
-);
+    'marque' => 'Lamborghini',
+    'puissance' => 700,
+    'prix' => 200000,
+    ),
+  );
 print_r($vehiculeConcession);
 echo '<br>';
 echo 'Le vehicule de bas de gamme s\'appelle '.$vehiculeConcession['Bas de gamme']['nom'].' de marque '.$vehiculeConcession['Bas de gamme']['marque'].' pour une puissance de '.
-      $vehiculeConcession['Bas de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Bas de gamme']['prix'].' Euros';
+$vehiculeConcession['Bas de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Bas de gamme']['prix'].' Euros';
 echo '<br>';
 echo 'Le vehicule de Milieu de gamme s\'appelle '.$vehiculeConcession['Milieu de gamme']['nom'].' de marque '.$vehiculeConcession['Milieu de gamme']['marque'].' pour une puissance de '.
-      $vehiculeConcession['Milieu de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Milieu de gamme']['prix'].' Euros';
+$vehiculeConcession['Milieu de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Milieu de gamme']['prix'].' Euros';
 echo '<br>';
 echo 'Le vehicule de Haut de gamme s\'appelle '.$vehiculeConcession['Haut de gamme']['nom'].' de marque '.$vehiculeConcession['Haut de gamme']['marque'].' pour une puissance de '.
-      $vehiculeConcession['Haut de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Haut de gamme']['prix'].' Euros';
+$vehiculeConcession['Haut de gamme']['puissance'].'cv et coute '.$vehiculeConcession['Haut de gamme']['prix'].' Euros';
